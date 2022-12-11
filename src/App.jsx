6b0bd19e2cad102b9966b07d461e98d03884bbe7
Harsh90Prajapati <harshprajapati904433@gmail.com>
2022-12-12 00:18:49 +0530

    git

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,87 @@
 import React from "react";
 import data from "./components/data.json";
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import { BsPlusLg } from "react-icons/bs";
+import { AiOutlineAmazon } from "react-icons/ai";
 
 const App = () => {
   return (
-    <div className="">
+    <div className="bg-white m-3 h-full p-2 rounded-lg drop-shadow-2xl ">
       {/*search area*/}
+      {/*product area*/}
+      <div className="m-6">
+        <nav className="w-full flex items-center sm:items-center justify-evenly gap-3 sm:px-5 sm:py-5">
+          <AiOutlineAmazon />
+          <input
+            type="text"
+            name="name"
+            placeholder="Amazon"
+            className="w-full border p-2 shadow-md rounded-md"
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="Flipkart"
+            className="w-full border p-2 hidden hover:border-black rounded-md "
+          />
+          <span className="shadow-md transition ease-in-out delay-150 bg-slate-800 hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 cursor-pointer rounded-md p-3">
+            <BsPlusLg className=" text-white text-xs" />
+          </span>
+          <button
+            className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-md text-white shadow-md shadow-blue-300"
+            type="submit"
+          >
+            Search
+          </button>
+        </nav>
+        {data.map((item) => {
+          return (
+            <div className="m-3 lg:flex flex flex-wrap justify-between lg:gap-1 gap-12 py-5">
+              <div className="flex flex-col lg:w-[600px] items-center gap-6">
+                <h1 className="lg:text-xl text-sm font-semibold p-2 text-left">
+                  {item.name}
+                  <hr />
+                </h1>
+                
+                <div className="bg-white lg:w-[580px] rounded-md shadow-md hover:drop-shadow-xl">
+                  <img
+                    src={item.image}
+                    alt="product"
+                    className="  rounded-md "
+                  />
+                </div>
+                <a
+                  href="https://www.amazon.in/"
+                  className="transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 p-2 rounded-lg text-white text-md shadow-md shadow-blue-300 w-1/2 text-center"
+                >
+                  Purchase
+                </a>
+              </div>
+              <div className="flex flex-col lg:w-[450px]  items-center justify-between">
+                <h1 className="lg:text-3xl text-normal font-semibold">{item.rate} </h1>
+                <hr />
+                <div className=" lg:w-[430px]">
+                  <span className="lg:text-3xl text-normal font-bold text-green-500 ">Good Battery Large screen Amazing pressure pen <span className=" text-red-500 ">too thick pen very slippery</span></span>
+                </div>
+               <div className="flex flex-col gap-3">
+                <div className="bg-white h-28 lg:w-[430px] rounded-md shadow-md">
+                  {item.p1}
+                </div>
+                <div className="bg-white h-28 lg:w-[430px] rounded-md shadow-md">
+                  {item.p1}
+                </div>
+                <div className="bg-white h-28 lg:w-[430px] rounded-md shadow-md">
+                  {item.p1}
+                </div>
+               </div>
+              </div>
 
-      <nav className="fixed z-[1] h-20 overflow-hidden top-0 bg-white dark:bg-slate-900  w-full flex items-center shadow-md sm:items-center justify-between gap-3 py-4 px-24 sm:px-5 sm:py-5">
-        <input
-          type="text"
-          name="name"
-          placeholder="Amazon"
-          className="w-full border p-2 hover:border-black rounded-md"
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Flipkart"
-          className="w-full border p-2 hover:border-black rounded-md "
-        />
-        <button
-          className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-md text-white shadow-md shadow-blue-300"
-          type="submit"
-        >
-          Search
-        </button>
-        <span className="transition ease-in-out shadow-md delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-green-400 duration-300 cursor-pointer rounded-full">
-          <AiOutlinePlusCircle className="text-3xl " />
-        </span>
-      </nav>
-     {/*product area*/}
-      <div className=" mt-24 mb-12 ">
-        {(data.map((item) => {
-          return(
-          <div className="bg-blue-400 m-10 h-full p-4 rounded-lg shadow-md ">
-          <h1 className="text-4xl font-semibold p-5">{item.name}</h1>
-          <hr />
-          <div className="m-3 flex items-center justify-between gap-4">
-            <div className="bg-white h-[450px] w-1/4 p-2 flex flex-col justify-between rounded-md shadow-md ">
-              <img src={item.image} alt="product" className=" h-fit shadow-md rounded-md"/>
-              <div className="flex flex-col gap-1">
-                <div className=" flex gap-2">
-                 <b>Name:-</b>
-                 <p className="text-sm font-normal">{item.name}</p>
-                </div>
-                <div className=" flex gap-2">
-                 <b>Price:-</b>
-                 <p className="text-sm font-normal">{item.rate}</p>
-                </div>
-                <div className=" flex gap-2">
-                 <b>Type:-</b>
-                 <p className="text-sm font-normal">{item.fuel_type}</p>
-                </div>
-                <div className=" flex gap-2">
-                 <b>Milage:-</b>
-                 <p className="text-sm font-normal">{item.milage}</p>
-                </div>
-                <div className=" flex gap-2">
-                 <b>Description:-</b>
-                 <p className="text-sm font-normal">{item.description}</p>
-                </div>
+              <div className="bg-white flex flex-col p-2 items-center justify-center">
+                <h1 className="lg:text-3xl text-normal font-bold ">Our Verdict <br /> <span className="text-green-500">{item.fuel_type}</span></h1>
+                pi chart
               </div>
             </div>
-            <div className="h-[450px] w-1/2 flex flex-col gap-3">
-              <div className="bg-white h-1/2 rounded-md shadow-md">Product detail</div>
-              <div className="bg-white h-1/2 rounded-md shadow-md">product review</div>
-            </div>
-            <div className="bg-white h-[450px] w-1/4 rounded-md shadow-md flex items-center justify-center">pi chart</div>
-          </div>
-          <a
-          href="https://www.amazon.in/"
-          className="transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 p-2 rounded-md text-white text-md shadow-md shadow-blue-300 m-3"
-          >
-          Purchase
-          </a>
-        </div>
-
-        )}))}
-        
-       
+          );
+        })}
       </div>
     </div>
   );
